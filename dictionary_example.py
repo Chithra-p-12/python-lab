6bab889b2e89d2403d6ea7e7ffd775f017e51101
Chithra-p-12 <pilegawdapilegawda@gmail.com>
2026-04-22 11:58:57 +0530

    Add files via upload

---
+++ dictionary_example.py
@@ -0,0 +1,13 @@
+student={"name": "alice",
+         "age":21,
+         "subjects":["math","physics"]
+         }
+print("dictionary:",student)
+
+employee={
+    "name":"chithra",
+    "id":120,
+    "department":"IT",
+    "salary":[50000]
+}
+print("dictionary:",employee)
